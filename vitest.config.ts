12b97d23508cf53f9903f2d3a68114162da5deaa
watagit <wtr0414@gmail.com>
2025-10-31 16:11:51 +0900

    esbuild を導入する

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,10 @@ export default defineConfig({
   test: {
     environment: 'jsdom',
     globals: true,
+    exclude: [
+      'node_modules/**',
+      'dist/**',
+    ],
     coverage: {
       provider: 'v8',
       reporter: ['text', 'json', 'html'],
@@ -16,4 +20,3 @@ export default defineConfig({
     },
   },
 });
-
